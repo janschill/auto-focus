66e156e647a228e48141fa2cc3fa4e1df4a574bb
janschill <janschill@proton.me>
2025-03-18 15:48:06 +0100

    Add boilerplate test dirs

---
+++ auto-focusTests/auto_focusTests.swift
@@ -0,0 +1,17 @@
+//
+//  auto_focusTests.swift
+//  auto-focusTests
+//
+//  Created by Jan Schill on 25/01/2025.
+//
+
+import Testing
+@testable import auto_focus
+
+struct auto_focusTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
