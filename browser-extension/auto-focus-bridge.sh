2683d33af6ad1cf293478835ba90574703e6fb2d
janschill <janschill@proton.me>
2025-06-15 17:42:25 +0200

    Remove native messaging

--- browser-extension/auto-focus-bridge.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Auto-Focus Browser Extension Bridge
-# This script handles native messaging for the Chrome extension
-
-NATIVE_HOST="/Users/janschill/code/janschill/auto-focus/browser-extension/native-host.swift"
-
-# Run the native messaging host
-exec "$NATIVE_HOST"
